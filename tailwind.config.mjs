f1b49f4a949531b7cc929a3c13bbdcfad00c6f1a
Pickle58 <jpilkington332@gmail.com>
2025-02-23 18:06:03 -0700

    fixed tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}",
+    "./components/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./layouts/**/*.{astro,html,js,jsx,ts,tsx}",
+    "./pages/**/*.{astro,html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- taiwind.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"], // ✅ Ensure Tailwind scans all files
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
